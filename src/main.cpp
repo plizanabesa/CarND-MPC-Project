b666a1f6e7c627727bc5d91e35523874fdb1b25f
Pedro Lizana <plizanabesa@gmail.com>
2017-06-17 20:02:20 -0400

    First modifications

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "MPC.h"
 #include "json.hpp"
+#include "matplotlibcpp.h"
+
+/* References
+ https://discussions.udacity.com/t/do-you-need-to-transform-coordiantes/256483/9
+ */
+
+namespace plt = matplotlibcpp;
 
 // for convenience
 using json = nlohmann::json;
@@ -100,6 +107,82 @@ int main() {
           */
           double steer_value;
           double throttle_value;
+            
+          // Transform reference trajectory points from map coordinate system to vehicle coordinate system
+            vector<double> ptsx_car(ptsx.size());
+            vector<double> ptsy_car(ptsy.size());
+            for (int i = 0; i < ptsx.size(); i++ )
+            {
+                // Center points to the vehicle position given by (px,py)
+                double x = ptsx[i] - px;
+                double y = ptsy[i] - py;
+                
+                // Rotate points so the x axis is in the direction of the vehicle heading (psi)
+                ptsx_car[i] = x * cos(-psi) - y * sin(-psi);
+                ptsy_car[i] = x * sin(-psi) + y * cos(-psi);
+            }
+            
+            auto coeffs = polyfit(ptsx_car, ptsy_car, 3);
+            
+            // TODO: use px or 0
+            
+            // The cross track error is calculated by evaluating at polynomial at x, f(x)
+            // and subtracting y.
+            double cte = polyeval(coeffs, px) - py;
+            // Due to the sign starting at 0, the orientation error is -f'(x).
+            // derivative of coeffs[0] + coeffs[1] * x -> coeffs[1]
+            double epsi = psi - atan(coeffs[1]);
+            
+            Eigen::VectorXd state(6);
+            state << px, py, psi, v, cte, epsi;
+            
+            std::vector<double> x_vals = {state[0]};
+            std::vector<double> y_vals = {state[1]};
+            std::vector<double> psi_vals = {state[2]};
+            std::vector<double> v_vals = {state[3]};
+            std::vector<double> cte_vals = {state[4]};
+            std::vector<double> epsi_vals = {state[5]};
+            std::vector<double> delta_vals = {};
+            std::vector<double> a_vals = {};
+            
+            auto vars = mpc.Solve(state, coeffs);
+                
+            x_vals.push_back(vars[0]);
+            y_vals.push_back(vars[1]);
+            psi_vals.push_back(vars[2]);
+            v_vals.push_back(vars[3]);
+            cte_vals.push_back(vars[4]);
+            epsi_vals.push_back(vars[5]);
+                
+            delta_vals.push_back(vars[6]);
+            a_vals.push_back(vars[7]);
+                
+            state << vars[0], vars[1], vars[2], vars[3], vars[4], vars[5];
+            std::cout << "x = " << vars[0] << std::endl;
+            std::cout << "y = " << vars[1] << std::endl;
+            std::cout << "psi = " << vars[2] << std::endl;
+            std::cout << "v = " << vars[3] << std::endl;
+            std::cout << "cte = " << vars[4] << std::endl;
+            std::cout << "epsi = " << vars[5] << std::endl;
+            std::cout << "delta = " << vars[6] << std::endl;
+            std::cout << "a = " << vars[7] << std::endl;
+            std::cout << std::endl;
+            
+            // Plot values
+            // NOTE: feel free to play around with this.
+            // It's useful for debugging!
+            plt::subplot(3, 1, 1);
+            plt::title("CTE");
+            plt::plot(cte_vals);
+            plt::subplot(3, 1, 2);
+            plt::title("Delta (Radians)");
+            plt::plot(delta_vals);
+            plt::subplot(3, 1, 3);
+            plt::title("Velocity");
+            plt::plot(v_vals);
+            
+            plt::show();
+            
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
